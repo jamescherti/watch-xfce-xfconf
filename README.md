8448e265cfc7cbad280862d21b5ade898b899b2d
James Cherti <60946298+jamescherti@users.noreply.github.com>
2021-06-02 14:14:32 -0400

    The first version of the command-line tool 'monitor-xfconf-changes'.

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # monitor-xfconf-changes
-'monitor-xfconf-changes' is a tool that can help you to configure 'XFCE 4' programmatically by displaying the 'xfconf-query' commands that correspond to any change made in 'XFCE 4' settings / 'Xfconf'.
+
+'monitor-xfconf-changes' is a command-line tool that can help you to configure XFCE 4 programmatically.
+
+It will display the 'xfconf-query' commands of all the settings that are modified by any software that modifies Xfconf: 'xfce4-settings-manager', 'thunar', 'catfish', 'ristretto'...
+
+You can then add the 'xfconf-query' commands to a Shell script that you can use to configure XFCE 4 programmatically.
+
+Configuring XFCE 4 programmatically is useful if you want to have the same configuration on several XFCE 4 desktops.
+
+## Author
+- [James Cherti](https://github.com/jamescherti/)
+
+## Requirements
+
+Python 3 requirements:
+- [psutil](https://pypi.org/project/psutil/)
+- [lxml](https://pypi.org/project/lxml/)
+- [mypy](https://pypi.org/project/mypy/)
+
+## Usage
+
+Clone the repository and enter the directory:
+```sh
+$ git clone https://github.com/jamescherti/monitor-xfconf-changes/
+$ cd monitor-xfconf-changes
+```
+
+Run 'xfce4-settings-manager' in the background:
+```sh
+$ xfce4-settings-manager &
+```
+
+Execute 'monitor-xfconf-settings.py':
+```sh
+$ ./monitor-xfconf-settings.py
+[INFO] You can start modifying XFCE 4 settings with xfce4-settings-manager. Your changes will be displayed in this terminal...
+```
+
+## Features:
+- Parse the XML files that are in the directory: '~/.config/xfce4/xfconf/xfce-perchannel-xml/',
+- Monitor changes in XFCE 4 settings / Xfconf,
+- Display 'xfconf-query' commands,
+- Supported Xfconf types: uint, int, string, bool, array, double.
+
+## Links:
+- 'monitor-xfconf-changes' GitHub repository: https://github.com/jamescherti/monitor-xfconf-changes/
+- More information about Xfconf: https://wiki.xfce.org/releng/4.6/general-info/
---
+++ monitor-xfconf-settings.py
@@ -0,0 +1,237 @@
+#!/usr/bin/env python
+#
+# monitor-xfconf-settings.py
+#
+# Copyright (c) 2021 James Cherti
+#
+# GitHub: https://github.com/jamescherti/monitor-xfconf-changes/
+#
+# Distributed under terms of the MIT license.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# --
+# pylint: disable=invalid-name
+#
+"""This script will help you to configure 'XFCE 4' programmatically.
+
+It will display the 'xfconf-query' commands of all the 'Xfconf' settings that
+are modified by 'xfce4-settings-manager' (or by any other software that
+modifies 'Xfconf' like 'Thunar', 'Catfish', 'Ristretto'...).
+
+You can then add the 'xfconf-query' commands to a Shell script that you can use
+to configure 'XFCE 4' programmatically.
+
+"""
+
+import os
+import signal
+import sys
+from typing import Set, Union, Any
+import time
+from pathlib import Path
+
+import psutil
+from lxml import etree as ETree
+
+
+__author__ = "James Cherti"
+__license__ = "MIT"
+__version__ = "1.0.0"
+
+
+class XfconfError(Exception):
+    """Exception raised by the class Xfconf() or its children."""
+
+
+class XfconfItem:
+    """Xfconf item."""
+
+    def __init__(self,
+                 channel: str,
+                 property_path: str,
+                 property_type: str,
+                 property_value: Union[str, list]):
+        """Init the class."""
+        self.channel = channel
+        self.property_path = property_path
+        self.property_type = property_type
+        self.property_value = property_value
+
+    def __repr__(self) -> str:
+        """Object representation in string format."""
+        result = "{}{} : {} = {}".format(self.channel,
+                                         self.property_path,
+                                         self.property_type,
+                                         self.property_value)
+        return result
+
+
+class Xfconf:
+    """Load Xfconf settings."""
+
+    @staticmethod
+    def escape_command(command: str) -> str:
+        """Quote a command."""
+        return "'{}'".format(command.replace("'", "'\\''"))
+
+    @staticmethod
+    def reload_xfconfd():
+        """Reload the process 'xfconfd'."""
+        for proc in psutil.process_iter():
+            try:
+                if proc.name() == "xfconfd":
+                    # reload the process
+                    os.kill(proc.pid, signal.SIGHUP)
+            except psutil.Error:
+                pass
+
+    def __init__(self):
+        """Load Xfconf settings."""
+        self.xfconf_items: set = set()
+        dir_xfconf = Path("~/.config/xfce4/xfconf/xfce-perchannel-xml")
+        for xml_file in dir_xfconf.expanduser().glob("*.xml"):
+            self._parse_xfconf_perchannel_xml(str(xml_file))
+
+    def diff(self) -> Set[str]:
+        """Return the settings that have been changed."""
+        Xfconf.reload_xfconfd()
+
+        new_xfce_config = Xfconf()
+
+        before = set(str(self).splitlines())
+        after = set(str(new_xfce_config).splitlines())
+
+        self.xfconf_items = new_xfce_config.xfconf_items
+        return after - before
+
+    def __iter__(self):
+        """Iterate through 'self.items'."""
+        yield from self.xfconf_items
+
+    def __repr__(self) -> str:
+        """Object representation in string format."""
+        commands = []
+
+        for item in self:
+            cmd = "{} --create -c {} -p {}" \
+                .format("xfconf-query",
+                        self.escape_command(item.channel),
+                        self.escape_command(item.property_path))
+
+            if item.property_type == "array":
+                for array_item_type, array_item_value in item.property_value:
+                    cmd =  "{} --type {} --set {}".format(
+                        cmd,
+                        self.escape_command(array_item_type),
+                        self.escape_command(array_item_value)
+                    )
+            else:
+                cmd =  "{} --type {} --set {}".format(
+                    cmd,
+                    self.escape_command(item.property_type),
+                    self.escape_command(str(item.property_value))
+                )
+
+            commands.append(cmd)
+
+        return "{}\n".format("\n".join(commands))
+
+    def _parse_xfconf_perchannel_xml(self,
+               xml_file: str,
+               root: Any = None,
+               channel_name: str = "",
+               property_path: str = ""):
+        """Parse the Xfconf XML."""
+        if root is None:
+            tree = ETree.parse(xml_file)
+            root = tree.getroot()
+
+            channel_name = root.attrib.get("name")
+            if root.attrib.get("version") != "1.0" \
+                    or root.tag.lower() != "channel":
+                err_str = ("invalid XML file: '{}'").format(xml_file)
+                raise XfconfError(err_str)
+
+        for elem in root.getchildren():
+            if elem.tag.title().lower() != "property":
+                continue
+
+            property_type = elem.attrib.get("type").strip().lower()
+            cur_property_path = "{}/{}".format(property_path,
+                                               elem.attrib.get("name").strip())
+
+            if property_type not in ["empty", "uint", "int", "string", "bool",
+                                     "array", "double"]:
+                err_str = ("the type '{}' of '{}{}' is not supported. "
+                           "XML file: '{}'") \
+                    .format(property_type, channel_name, cur_property_path,
+                            xml_file)
+                raise XfconfError(err_str)
+
+            # 'empty' = contains sub items
+            if property_type == "empty":
+                self._parse_xfconf_perchannel_xml(xml_file=xml_file,
+                            root=elem,
+                            channel_name=channel_name,
+                            property_path=cur_property_path)
+                continue
+
+            # Modify the variable property_value
+            if property_type == "array":
+                property_value = []
+                for elem_property_value in elem.getchildren():
+                    array_item_type = elem_property_value.attrib.get("type")
+                    array_item_value = elem_property_value.attrib.get("value")
+                    if array_item_value is None:
+                        array_item_value = ""
+                    property_value.append((array_item_type, array_item_value))
+            else:
+                property_value = elem.attrib.get("value")
+
+            self.xfconf_items.add(
+                XfconfItem(
+                    channel=channel_name,
+                    property_path=cur_property_path,
+                    property_type=property_type,
+                    property_value=property_value
+                )
+            )
+
+
+def main():
+    """The program starts here."""
+
+    print("[INFO] You can start modifying 'XFCE 4' settings with "
+          "'xfce4-settings-manager'. Your changes will be displayed in this "
+          "terminal...\n", file=sys.stderr)
+
+    try:
+        xfconf_query_list = Xfconf()
+        while True:
+            time.sleep(1)
+            delta = xfconf_query_list.diff()
+            for item in delta:
+                print(item)
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == '__main__':
+    main()
